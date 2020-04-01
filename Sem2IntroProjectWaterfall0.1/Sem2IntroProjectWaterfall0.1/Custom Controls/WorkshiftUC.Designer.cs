ba881411b215dc5b76718544eed8f39fc1714cb8
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-04-01 23:19:28 +0200

    Added a basic implementation of workshift user controls

---
+++ Sem2IntroProjectWaterfall0.1/Sem2IntroProjectWaterfall0.1/Custom Controls/WorkshiftUC.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Sem2IntroProjectWaterfall0._1
+{
+    partial class WorkshiftUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.panelShiftTwo = new System.Windows.Forms.Panel();
+            this.panelShiftOne = new System.Windows.Forms.Panel();
+            this.panelShiftThree = new System.Windows.Forms.Panel();
+            this.panelDivider = new System.Windows.Forms.Panel();
+            this.lblShiftStart = new System.Windows.Forms.Label();
+            this.lblShiftMidpointOne = new System.Windows.Forms.Label();
+            this.lblShiftMidpointTwo = new System.Windows.Forms.Label();
+            this.lblShiftEnd = new System.Windows.Forms.Label();
+            this.panelDateWrapper = new System.Windows.Forms.Panel();
+            this.panelWrapperContents = new System.Windows.Forms.Panel();
+            this.panelDateWrapper.SuspendLayout();
+            this.panelWrapperContents.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(32, 3);
+            this.lblName.Name = "lblName";
+            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblName.Size = new System.Drawing.Size(170, 18);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "<Placeholder>";
+            // 
+            // panelShiftTwo
+            // 
+            this.panelShiftTwo.AutoSize = true;
+            this.panelShiftTwo.BackColor = System.Drawing.SystemColors.Control;
+            this.panelShiftTwo.Location = new System.Drawing.Point(422, 4);
+            this.panelShiftTwo.MinimumSize = new System.Drawing.Size(205, 19);
+            this.panelShiftTwo.Name = "panelShiftTwo";
+            this.panelShiftTwo.Size = new System.Drawing.Size(214, 19);
+            this.panelShiftTwo.TabIndex = 4;
+            // 
+            // panelShiftOne
+            // 
+            this.panelShiftOne.AutoSize = true;
+            this.panelShiftOne.BackColor = System.Drawing.SystemColors.Control;
+            this.panelShiftOne.Location = new System.Drawing.Point(214, 4);
+            this.panelShiftOne.MinimumSize = new System.Drawing.Size(205, 19);
+            this.panelShiftOne.Name = "panelShiftOne";
+            this.panelShiftOne.Size = new System.Drawing.Size(210, 19);
+            this.panelShiftOne.TabIndex = 5;
+            // 
+            // panelShiftThree
+            // 
+            this.panelShiftThree.AutoSize = true;
+            this.panelShiftThree.BackColor = System.Drawing.SystemColors.Control;
+            this.panelShiftThree.Location = new System.Drawing.Point(635, 4);
+            this.panelShiftThree.MinimumSize = new System.Drawing.Size(205, 19);
+            this.panelShiftThree.Name = "panelShiftThree";
+            this.panelShiftThree.Size = new System.Drawing.Size(219, 19);
+            this.panelShiftThree.TabIndex = 6;
+            // 
+            // panelDivider
+            // 
+            this.panelDivider.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panelDivider.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDivider.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelDivider.Location = new System.Drawing.Point(0, 43);
+            this.panelDivider.Name = "panelDivider";
+            this.panelDivider.Size = new System.Drawing.Size(871, 2);
+            this.panelDivider.TabIndex = 7;
+            // 
+            // lblShiftStart
+            // 
+            this.lblShiftStart.AutoSize = true;
+            this.lblShiftStart.Location = new System.Drawing.Point(211, 0);
+            this.lblShiftStart.Name = "lblShiftStart";
+            this.lblShiftStart.Size = new System.Drawing.Size(36, 17);
+            this.lblShiftStart.TabIndex = 8;
+            this.lblShiftStart.Text = "0:00";
+            // 
+            // lblShiftMidpointOne
+            // 
+            this.lblShiftMidpointOne.AutoSize = true;
+            this.lblShiftMidpointOne.Location = new System.Drawing.Point(405, 0);
+            this.lblShiftMidpointOne.Name = "lblShiftMidpointOne";
+            this.lblShiftMidpointOne.Size = new System.Drawing.Size(36, 17);
+            this.lblShiftMidpointOne.TabIndex = 9;
+            this.lblShiftMidpointOne.Text = "0:00";
+            // 
+            // lblShiftMidpointTwo
+            // 
+            this.lblShiftMidpointTwo.AutoSize = true;
+            this.lblShiftMidpointTwo.Location = new System.Drawing.Point(617, 0);
+            this.lblShiftMidpointTwo.Name = "lblShiftMidpointTwo";
+            this.lblShiftMidpointTwo.Size = new System.Drawing.Size(36, 17);
+            this.lblShiftMidpointTwo.TabIndex = 10;
+            this.lblShiftMidpointTwo.Text = "0:00";
+            // 
+            // lblShiftEnd
+            // 
+            this.lblShiftEnd.Location = new System.Drawing.Point(802, 0);
+            this.lblShiftEnd.Name = "lblShiftEnd";
+            this.lblShiftEnd.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblShiftEnd.Size = new System.Drawing.Size(55, 17);
+            this.lblShiftEnd.TabIndex = 11;
+            this.lblShiftEnd.Text = "0:00";
+            // 
+            // panelDateWrapper
+            // 
+            this.panelDateWrapper.AutoSize = true;
+            this.panelDateWrapper.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelDateWrapper.Controls.Add(this.lblShiftStart);
+            this.panelDateWrapper.Controls.Add(this.lblShiftEnd);
+            this.panelDateWrapper.Controls.Add(this.lblShiftMidpointOne);
+            this.panelDateWrapper.Controls.Add(this.lblShiftMidpointTwo);
+            this.panelDateWrapper.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDateWrapper.Location = new System.Drawing.Point(0, 0);
+            this.panelDateWrapper.Name = "panelDateWrapper";
+            this.panelDateWrapper.Size = new System.Drawing.Size(871, 17);
+            this.panelDateWrapper.TabIndex = 12;
+            // 
+            // panelWrapperContents
+            // 
+            this.panelWrapperContents.Controls.Add(this.lblName);
+            this.panelWrapperContents.Controls.Add(this.panelShiftOne);
+            this.panelWrapperContents.Controls.Add(this.panelShiftTwo);
+            this.panelWrapperContents.Controls.Add(this.panelShiftThree);
+            this.panelWrapperContents.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelWrapperContents.Location = new System.Drawing.Point(0, 17);
+            this.panelWrapperContents.Name = "panelWrapperContents";
+            this.panelWrapperContents.Size = new System.Drawing.Size(871, 26);
+            this.panelWrapperContents.TabIndex = 13;
+            // 
+            // WorkshiftUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.panelWrapperContents);
+            this.Controls.Add(this.panelDateWrapper);
+            this.Controls.Add(this.panelDivider);
+            this.MinimumSize = new System.Drawing.Size(871, 30);
+            this.Name = "WorkshiftUC";
+            this.Size = new System.Drawing.Size(871, 45);
+            this.panelDateWrapper.ResumeLayout(false);
+            this.panelDateWrapper.PerformLayout();
+            this.panelWrapperContents.ResumeLayout(false);
+            this.panelWrapperContents.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel panelShiftTwo;
+        private System.Windows.Forms.Panel panelShiftOne;
+        private System.Windows.Forms.Panel panelShiftThree;
+        private System.Windows.Forms.Panel panelDivider;
+        private System.Windows.Forms.Label lblShiftStart;
+        private System.Windows.Forms.Label lblShiftMidpointOne;
+        private System.Windows.Forms.Label lblShiftMidpointTwo;
+        private System.Windows.Forms.Label lblShiftEnd;
+        private System.Windows.Forms.Panel panelDateWrapper;
+        private System.Windows.Forms.Panel panelWrapperContents;
+    }
+}
---
+++ Sem2IntroProjectWaterfall0.1/Sem2IntroProjectWaterfall0.1/WorkshiftUnit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem2IntroProjectWaterfall0._1
+{
+    public class WorkshiftUnit
+    {
+        //Instance variable(s)
+        private Employee employee;
+        private int workshift;
+        private int status;
+
+        //Properties
+        public Employee Employee
+        {
+            get { return this.employee; }
+        }
+
+        public int Workshift
+        {
+            get { return this.workshift; }
+            set
+            {
+                if (value >= 0 && value <= 2) this.workshift = value;
+                else this.workshift = 0;
+            }
+        }
+
+        public int Status
+        {
+            get { return this.status; }
+            set
+            {
+                if (value >= 0 && value <= 3) this.status = value;
+                else this.status = 0;
+            }
+        }
+
+        //Constructor(s)
+        public WorkshiftUnit(Employee newEmployee, int workshift, int status)
+        {
+            this.employee = newEmployee;
+            this.Workshift = workshift;
+            this.Status = status;
+        }
+    }
+}
