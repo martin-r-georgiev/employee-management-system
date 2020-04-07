bc72967de89ea0ef8b0a01a10894922cd1b6cbaa
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-04-07 11:53:34 +0200

    Saved changes in order to pull from remote repository

---
+++ Sem2IntroProjectWaterfall0.1/Sem2IntroProjectWaterfall0.1/WorkshiftWeeklyUC.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Sem2IntroProjectWaterfall0._1
+{
+    partial class WorkshiftWeeklyUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.panelDivider = new System.Windows.Forms.Panel();
+            this.panelContents = new System.Windows.Forms.Panel();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblMonday = new System.Windows.Forms.Label();
+            this.lblMondayDate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTuesday = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWednesday = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblThursday = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panelContents.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(1, 7);
+            this.lblName.Name = "lblName";
+            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblName.Size = new System.Drawing.Size(163, 18);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "<Placeholder>";
+            // 
+            // panelDivider
+            // 
+            this.panelDivider.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panelDivider.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDivider.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelDivider.Location = new System.Drawing.Point(0, 90);
+            this.panelDivider.Name = "panelDivider";
+            this.panelDivider.Size = new System.Drawing.Size(930, 2);
+            this.panelDivider.TabIndex = 8;
+            // 
+            // panelContents
+            // 
+            this.panelContents.Controls.Add(this.lblName);
+            this.panelContents.Location = new System.Drawing.Point(0, 53);
+            this.panelContents.Name = "panelContents";
+            this.panelContents.Size = new System.Drawing.Size(930, 31);
+            this.panelContents.TabIndex = 9;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.label11);
+            this.panelHeader.Controls.Add(this.label12);
+            this.panelHeader.Controls.Add(this.label9);
+            this.panelHeader.Controls.Add(this.label10);
+            this.panelHeader.Controls.Add(this.label7);
+            this.panelHeader.Controls.Add(this.label8);
+            this.panelHeader.Controls.Add(this.label5);
+            this.panelHeader.Controls.Add(this.lblThursday);
+            this.panelHeader.Controls.Add(this.label3);
+            this.panelHeader.Controls.Add(this.lblWednesday);
+            this.panelHeader.Controls.Add(this.label1);
+            this.panelHeader.Controls.Add(this.lblTuesday);
+            this.panelHeader.Controls.Add(this.lblMondayDate);
+            this.panelHeader.Controls.Add(this.lblMonday);
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(930, 47);
+            this.panelHeader.TabIndex = 10;
+            // 
+            // lblMonday
+            // 
+            this.lblMonday.AutoSize = true;
+            this.lblMonday.Location = new System.Drawing.Point(196, 9);
+            this.lblMonday.Name = "lblMonday";
+            this.lblMonday.Size = new System.Drawing.Size(58, 17);
+            this.lblMonday.TabIndex = 0;
+            this.lblMonday.Text = "Monday";
+            // 
+            // lblMondayDate
+            // 
+            this.lblMondayDate.AutoSize = true;
+            this.lblMondayDate.Location = new System.Drawing.Point(176, 26);
+            this.lblMondayDate.Name = "lblMondayDate";
+            this.lblMondayDate.Size = new System.Drawing.Size(99, 17);
+            this.lblMondayDate.TabIndex = 1;
+            this.lblMondayDate.Text = "<Placeholder>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "<Placeholder>";
+            // 
+            // lblTuesday
+            // 
+            this.lblTuesday.AutoSize = true;
+            this.lblTuesday.Location = new System.Drawing.Point(304, 9);
+            this.lblTuesday.Name = "lblTuesday";
+            this.lblTuesday.Size = new System.Drawing.Size(58, 17);
+            this.lblTuesday.TabIndex = 2;
+            this.lblTuesday.Text = "Monday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "<Placeholder>";
+            // 
+            // lblWednesday
+            // 
+            this.lblWednesday.AutoSize = true;
+            this.lblWednesday.Location = new System.Drawing.Point(411, 9);
+            this.lblWednesday.Name = "lblWednesday";
+            this.lblWednesday.Size = new System.Drawing.Size(58, 17);
+            this.lblWednesday.TabIndex = 4;
+            this.lblWednesday.Text = "Monday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(496, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "<Placeholder>";
+            // 
+            // lblThursday
+            // 
+            this.lblThursday.AutoSize = true;
+            this.lblThursday.Location = new System.Drawing.Point(516, 9);
+            this.lblThursday.Name = "lblThursday";
+            this.lblThursday.Size = new System.Drawing.Size(58, 17);
+            this.lblThursday.TabIndex = 6;
+            this.lblThursday.Text = "Monday";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(601, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "<Placeholder>";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(621, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Monday";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(706, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 17);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "<Placeholder>";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(726, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 17);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Monday";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(811, 26);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(99, 17);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "<Placeholder>";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(831, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 17);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Monday";
+            // 
+            // WorkshiftWeeklyUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelHeader);
+            this.Controls.Add(this.panelContents);
+            this.Controls.Add(this.panelDivider);
+            this.Name = "WorkshiftWeeklyUC";
+            this.Size = new System.Drawing.Size(930, 92);
+            this.panelContents.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel panelDivider;
+        private System.Windows.Forms.Panel panelContents;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblMonday;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblThursday;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWednesday;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTuesday;
+        private System.Windows.Forms.Label lblMondayDate;
+    }
+}
---
+++ Sem2IntroProjectWaterfall0.1/Sem2IntroProjectWaterfall0.1/WorkshiftWeeklyUC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sem2IntroProjectWaterfall0._1
+{
+    public partial class WorkshiftWeeklyUC : UserControl
+    {
+        public WorkshiftWeeklyUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
