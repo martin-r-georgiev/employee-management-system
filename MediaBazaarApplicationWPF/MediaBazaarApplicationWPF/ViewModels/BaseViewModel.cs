5fe426686165e7bba04b0260e39eb2ba45f73c2a
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-04-30 21:16:04 +0200

    Added base WPF application version

---
+++ MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MediaBazaarApplicationWPF.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public class DelegateCommand : ICommand
+        {
+            private readonly Action<object> _executeAction;
+            private readonly Func<object, bool> _canExecuteAction;
+
+            public DelegateCommand(Action<object> executeAction, Func<object, bool> canExecuteAction)
+            {
+                _executeAction = executeAction;
+                _canExecuteAction = canExecuteAction;
+            }
+
+            public void Execute(object parameter) => _executeAction(parameter);
+
+            public bool CanExecute(object parameter) => _canExecuteAction?.Invoke(parameter) ?? true;
+
+            public event EventHandler CanExecuteChanged;
+
+            public void InvokeCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MediaBazaarApplicationWPF.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        //Instance variable(s)
+        private string _username;
+        private string _password;
+        //private readonly DelegateCommand _changeNameCommand;
+        //public ICommand ChangeNameCommand => _changeNameCommand;
+
+        private readonly DelegateCommand _loginCommand;
+        public ICommand LoginCommand => _loginCommand;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+                _loginCommand.InvokeCanExecuteChanged();
+                //Alternative to the 2 lines above: SetProperty(ref _username, value);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+                _loginCommand.InvokeCanExecuteChanged();
+                //Alternative to the 2 lines above: SetProperty(ref _password, value);
+            }
+        }
+
+        public LoginViewModel()
+        {
+            //_changeNameCommand = new DelegateCommand(OnChangeName, CanChangeName);
+            _loginCommand = new DelegateCommand(OnLogin, CanClickLogin);
+        }
+
+        private void OnLogin(object commandParameter)
+        {
+            Console.WriteLine($"Tried to login with credentials: {this.Username}, {this.Password}");
+        }
+
+        private bool CanClickLogin(object commandParameter)
+        {
+            return !String.IsNullOrWhiteSpace(this.Username) && !String.IsNullOrWhiteSpace(this.Password);
+        }
+
+        //private void OnChangeName(object commandParameter)
+        //{
+        //    //FirstName = "Test";
+        //    //_changeNameCommand.InvokeCanExecuteChanged(); Ability to toggle whether a command is enabled or disabled
+        //}
+
+        //private bool CanChangeName(object commandParameter)
+        //{
+        //    //return FirstName != "Test";
+        //    return true;
+        //}
+    }
+}
