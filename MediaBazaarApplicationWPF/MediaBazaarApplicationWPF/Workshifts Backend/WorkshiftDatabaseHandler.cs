05de2e6a6ec2d29596b3f06dedda9fb999045dd2
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-06-22 22:23:24 +0200

    Slight changes to WorkshiftDatabaseHandler

--- MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/Workshifts Backend/WorkshiftDatabaseHandler.cs
+++ MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/Workshifts Backend/WorkshiftDatabaseHandler.cs
@@ -15,96 +15,96 @@ namespace MediaBazaarApplicationWPF
         {
             using (MySqlConnection conn = SqlConnectionHandler.GetSqlConnection())
             {
-                List<int> missedWorkshiftIds = new List<int>();
-                DateTime startDate = DateTimeControls.StartOfWeek(date, DayOfWeek.Monday);
-                DateTime endDate = new DateTime();
-                if (startDate.AddDays(6) <= DateTime.Today) endDate = startDate.AddDays(6);
-                else endDate = DateTime.Today;
-
-                List<string> checkinUsers = new List<string>();
-                List<DateTime> checkinsList = new List<DateTime>();
-                List<DateTime> checkoutsList = new List<DateTime>();
-
-                using (MySqlCommand cmd = new MySqlCommand($"SELECT DISTINCT c.userID, MIN(c.checkin), MAX(c.checkout) FROM checkins as c INNER JOIN users as u ON c.userID=u.userID WHERE c.checkin >= @start AND c.checkout <= @end AND u.departmentID = @department GROUP BY c.userID, CAST(c.checkin as DATE)", conn))
+                try
                 {
-                    cmd.Parameters.AddWithValue("@start", startDate);
-                    cmd.Parameters.AddWithValue("@end", endDate);
-                    cmd.Parameters.AddWithValue("@department", departmentID);
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
-                    while (dataReader.Read())
+                    List<int> missedWorkshiftIds = new List<int>();
+                    DateTime startDate = DateTimeControls.StartOfWeek(date, DayOfWeek.Monday);
+                    DateTime endDate = (startDate.AddDays(6) <= DateTime.Today) ? startDate.AddDays(6) : DateTime.Today;
+
+                    List<string> checkinUsers = new List<string>();
+                    List<DateTime> checkinsList = new List<DateTime>();
+                    List<DateTime> checkoutsList = new List<DateTime>();
+
+                    using (MySqlCommand cmd = new MySqlCommand($"SELECT DISTINCT c.userID, MIN(c.checkin), MAX(c.checkout) FROM checkins as c INNER JOIN users as u ON c.userID=u.userID WHERE c.checkin >= @start AND c.checkout <= @end AND u.departmentID = @department GROUP BY c.userID, CAST(c.checkin as DATE)", conn))
                     {
-                        DateTime checkOut;
-                        if (!dataReader.IsDBNull(2)) checkOut = dataReader.GetDateTime(2);
-                        else checkOut = dataReader.GetDateTime(1).Date.AddDays(1).AddSeconds(-1);
-                        checkinUsers.Add(dataReader.GetString(0));
-                        checkinsList.Add(dataReader.GetDateTime(1));
-                        checkoutsList.Add(checkOut);
-                    }
-                    dataReader.Dispose();
-                    cmd.Dispose();
-                }
-
-                using (MySqlCommand cmd = new MySqlCommand($"SELECT w.userID, w.date, MIN(w.workshift), MAX(w.workshift), GROUP_CONCAT(w.id ORDER BY w.workshift) FROM workshifts as w INNER JOIN users as u ON w.userID=u.userID WHERE w.date >= @start AND w.date < @end AND u.departmentID = @department GROUP BY w.userID, w.date", conn))
-                {
-                    cmd.Parameters.AddWithValue("@start", startDate);
-                    cmd.Parameters.AddWithValue("@end", endDate);
-                    cmd.Parameters.AddWithValue("@department", departmentID);
-
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
-                    while (dataReader.Read())
-                    {
-                        string userID = dataReader.GetString(0);
-                        DateTime timestamp = dataReader.GetDateTime(1);
-                        int workshiftMinIndex = dataReader.GetInt16(2);
-                        int workshiftMaxIndex = dataReader.GetInt16(3);
-                        string rowIDRaw = dataReader.GetString(4);
-                        rowIDRaw = rowIDRaw.Replace("(", string.Empty).Replace(")", string.Empty);
-                        int[] rowIDs = Array.ConvertAll(rowIDRaw.Split(',').ToArray(), int.Parse);
-
-                        int index = checkinUsers.FindIndex(item => item == userID);
-                        if (index != -1 && checkinsList[index].Date == timestamp.Date)
+                        cmd.Parameters.AddWithValue("@start", startDate);
+                        cmd.Parameters.AddWithValue("@end", endDate);
+                        cmd.Parameters.AddWithValue("@department", departmentID);
+                        MySqlDataReader dataReader = cmd.ExecuteReader();
+                        while (dataReader.Read())
                         {
-                            int workshiftIndex = workshiftMinIndex;
-                            int counter = 0;
-
-                            while (workshiftIndex <= workshiftMaxIndex)
-                            {
-                                if (checkinsList[index] > timestamp.AddHours(9 + workshiftIndex * 4).AddMinutes(15))
-                                {
-                                    missedWorkshiftIds.Add(rowIDs[counter]);
-                                    counter++;
-                                }
-                                workshiftIndex++;
-                            }
-                            if (checkoutsList[index].Date == checkinsList[index].Date && checkoutsList[index] < timestamp.AddHours(13 + workshiftMaxIndex * 4).AddMinutes(-15))
-                            {
-                                missedWorkshiftIds.Add(rowIDs[rowIDs.Length - 1]);
-                            }
-                            checkinUsers.RemoveAt(index);
-                            checkinsList.RemoveAt(index);
-                            checkoutsList.RemoveAt(index);
+                            DateTime checkOut = (!dataReader.IsDBNull(2)) ? dataReader.GetDateTime(2) : dataReader.GetDateTime(1).Date.AddDays(1).AddSeconds(-1);
+                            checkinUsers.Add(dataReader.GetString(0));
+                            checkinsList.Add(dataReader.GetDateTime(1));
+                            checkoutsList.Add(checkOut);
                         }
-                        else foreach (int rowID in rowIDs) missedWorkshiftIds.Add(rowID);
-                    }
-                    dataReader.Dispose();
-                    cmd.Dispose();
-                }
-
-                missedWorkshiftIds = missedWorkshiftIds.Distinct().ToList();
-
-                foreach (int rowID in missedWorkshiftIds)
-                {
-                    using (MySqlCommand cmd = new MySqlCommand($"UPDATE workshifts SET status = @newStatus WHERE id = @rowID AND status IN (@available, @pending)", conn))
-                    {
-                        cmd.Parameters.AddWithValue("@newStatus", 3);
-                        cmd.Parameters.AddWithValue("@rowID", rowID);
-                        cmd.Parameters.AddWithValue("@available", 0);
-                        cmd.Parameters.AddWithValue("@pending", 1);
-                        cmd.ExecuteNonQuery();
+                        dataReader.Dispose();
                         cmd.Dispose();
                     }
+
+                    using (MySqlCommand cmd = new MySqlCommand($"SELECT w.userID, w.date, MIN(w.workshift), MAX(w.workshift), GROUP_CONCAT(w.id ORDER BY w.workshift) FROM workshifts as w INNER JOIN users as u ON w.userID=u.userID WHERE w.date >= @start AND w.date < @end AND u.departmentID = @department GROUP BY w.userID, w.date", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@start", startDate);
+                        cmd.Parameters.AddWithValue("@end", endDate);
+                        cmd.Parameters.AddWithValue("@department", departmentID);
+
+                        MySqlDataReader dataReader = cmd.ExecuteReader();
+                        while (dataReader.Read())
+                        {
+                            string userID = dataReader.GetString(0);
+                            DateTime timestamp = dataReader.GetDateTime(1);
+                            int workshiftMinIndex = dataReader.GetInt16(2);
+                            int workshiftMaxIndex = dataReader.GetInt16(3);
+                            string rowIDRaw = dataReader.GetString(4);
+                            rowIDRaw = rowIDRaw.Replace("(", string.Empty).Replace(")", string.Empty);
+                            int[] rowIDs = Array.ConvertAll(rowIDRaw.Split(',').ToArray(), int.Parse);
+
+                            int index = checkinUsers.FindIndex(item => item == userID);
+                            if (index != -1 && checkinsList[index].Date == timestamp.Date)
+                            {
+                                int workshiftIndex = workshiftMinIndex;
+                                int counter = 0;
+
+                                while (workshiftIndex <= workshiftMaxIndex)
+                                {
+                                    if (checkinsList[index] > timestamp.AddHours(9 + workshiftIndex * 4).AddMinutes(15))
+                                    {
+                                        missedWorkshiftIds.Add(rowIDs[counter]);
+                                        counter++;
+                                    }
+                                    workshiftIndex++;
+                                }
+                                if (checkoutsList[index].Date == checkinsList[index].Date && checkoutsList[index] < timestamp.AddHours(13 + workshiftMaxIndex * 4).AddMinutes(-15))
+                                {
+                                    missedWorkshiftIds.Add(rowIDs[rowIDs.Length - 1]);
+                                }
+                                checkinUsers.RemoveAt(index);
+                                checkinsList.RemoveAt(index);
+                                checkoutsList.RemoveAt(index);
+                            }
+                            else foreach (int rowID in rowIDs) missedWorkshiftIds.Add(rowID);
+                        }
+                        dataReader.Dispose();
+                        cmd.Dispose();
+                    }
+
+                    missedWorkshiftIds = missedWorkshiftIds.Distinct().ToList();
+
+                    foreach (int rowID in missedWorkshiftIds)
+                    {
+                        using (MySqlCommand cmd = new MySqlCommand($"UPDATE workshifts SET status = @newStatus WHERE id = @rowID AND status IN (@available, @pending)", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@newStatus", (int)WorkshiftStatus.Missed);
+                            cmd.Parameters.AddWithValue("@rowID", rowID);
+                            cmd.Parameters.AddWithValue("@available", (int)WorkshiftStatus.Available);
+                            cmd.Parameters.AddWithValue("@pending", (int)WorkshiftStatus.Pending);
+                            cmd.ExecuteNonQuery();
+                            cmd.Dispose();
+                        }
+                    }
                 }
-                conn.Close();
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally { conn.Close(); }
             }
         }
 
@@ -115,7 +115,7 @@ namespace MediaBazaarApplicationWPF
             {
                 try
                 {
-                    using (MySqlCommand cmd = new MySqlCommand($"SELECT w.userID, w.workshift, w.status FROM workshifts as w INNER JOIN users as u ON w.userID = u.userID WHERE date=@date AND u.departmentID = @department", conn))
+                    using (MySqlCommand cmd = new MySqlCommand($"SELECT w.userID, w.workshift, w.status FROM workshifts as w INNER JOIN users as u ON w.userID = u.userID WHERE w.date=@date AND u.departmentID = @department", conn))
                     {
                         cmd.Parameters.AddWithValue("@date", date.Date);
                         cmd.Parameters.AddWithValue("@department", departmentID);
@@ -125,12 +125,9 @@ namespace MediaBazaarApplicationWPF
                             int index = items.IndexOf(items.Where(item => item.Employee.UserID == dataReader.GetString(0)).FirstOrDefault());
                             if (index == -1)
                             {
-                                bool addEmployee = true;
                                 Employee newEmployee = EmployeeDatabaseHandler.GetEmployee(dataReader.GetString(0), false);
 
-                                if (filter.GetValue(newEmployee.Role) != true) addEmployee = false;
-
-                                if (addEmployee)
+                                if (filter.GetValue(newEmployee.Role) == true)
                                 {
                                     DailyWorkshift newUnit = new DailyWorkshift(newEmployee, date);
                                     newUnit.SetStatus(dataReader.GetInt16(2), dataReader.GetInt16(1));
@@ -142,14 +139,9 @@ namespace MediaBazaarApplicationWPF
                         dataReader.Dispose();
                         cmd.Dispose();
                     }
-                    conn.Close();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    //LoginScreen.logger.Log($"[WorkshiftDatabaseHandler] The application encountered an issue when trying to get data from database:\n{ex.Message}", LoggingLevels.ERROR);
-                    conn.Close();
-                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally { conn.Close(); }
             }
             return items;
         }
@@ -174,12 +166,9 @@ namespace MediaBazaarApplicationWPF
                             int index = items.IndexOf(items.Where(item => item.Employee.UserID == dataReader.GetString(0)).FirstOrDefault());
                             if (index == -1)
                             {
-                                bool addEmployee = true;
                                 Employee newEmployee = EmployeeDatabaseHandler.GetEmployee(dataReader.GetString(0), false);
 
-                                if (filter.GetValue(newEmployee.Role) != true) addEmployee = false;
-
-                                if (addEmployee)
+                                if (filter.GetValue(newEmployee.Role) == true)
                                 {
                                     WeeklyWorkshift newUnit = new WeeklyWorkshift(newEmployee, startDate, endDate);
                                     newUnit.SetStatus(dataReader.GetDateTime(3).DayOfWeek, dataReader.GetInt16(2), dataReader.GetInt16(1));
@@ -191,14 +180,9 @@ namespace MediaBazaarApplicationWPF
                         dataReader.Dispose();
                         cmd.Dispose();
                     }
-                    conn.Close();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    //LoginScreen.logger.Log($"[WorkshiftDatabaseHandler] The application encountered an issue when trying to get data from database:\n{ex.Message}", LoggingLevels.ERROR);
-                    conn.Close();
-                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally { conn.Close(); }
             }
             return items;
         }
@@ -207,29 +191,29 @@ namespace MediaBazaarApplicationWPF
         {
             using (MySqlConnection conn = SqlConnectionHandler.GetSqlConnection())
             {
-                using (MySqlCommand cmd = new MySqlCommand($"DELETE FROM workshifts WHERE userID=@userid AND date = @date AND workshift = @workshift", conn))
+                try
                 {
-                    cmd.Parameters.AddWithValue("@userid", employee.UserID);
-                    cmd.Parameters.AddWithValue("@date", date.Date);
-                    cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
-                    cmd.ExecuteNonQuery();
-                    cmd.Dispose();
-                }
-                conn.Close();
-            }
+                    using (MySqlCommand cmd = new MySqlCommand($"DELETE FROM workshifts WHERE userID=@userid AND date = @date AND workshift = @workshift", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@userid", employee.UserID);
+                        cmd.Parameters.AddWithValue("@date", date.Date);
+                        cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
+                        cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+                    }
 
-            using (MySqlConnection conn = SqlConnectionHandler.GetSqlConnection())
-            {
-                using (MySqlCommand cmd = new MySqlCommand($"INSERT IGNORE INTO workshifts (userID, date, workshift, status) VALUES (@userid, @date, @workshift, @status)", conn))
-                {
-                    cmd.Parameters.AddWithValue("@userid", employee.UserID);
-                    cmd.Parameters.AddWithValue("@date", date.Date);
-                    cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
-                    cmd.Parameters.AddWithValue("@status", status);
-                    cmd.ExecuteNonQuery();
-                    cmd.Dispose();
+                    using (MySqlCommand cmd = new MySqlCommand($"INSERT IGNORE INTO workshifts (userID, date, workshift, status) VALUES (@userid, @date, @workshift, @status)", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@userid", employee.UserID);
+                        cmd.Parameters.AddWithValue("@date", date.Date);
+                        cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
+                        cmd.Parameters.AddWithValue("@status", status);
+                        cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+                    }
                 }
-                conn.Close();
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally { conn.Close(); }
             }
         }
 
@@ -237,15 +221,19 @@ namespace MediaBazaarApplicationWPF
         {
             using (MySqlConnection conn = SqlConnectionHandler.GetSqlConnection())
             {
-                using (MySqlCommand cmd = new MySqlCommand($"DELETE FROM workshifts WHERE userID=@userid AND date = @date AND workshift = @workshift", conn))
+                try
                 {
-                    cmd.Parameters.AddWithValue("@userid", employee.UserID);
-                    cmd.Parameters.AddWithValue("@date", date.Date);
-                    cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
-                    cmd.ExecuteNonQuery();
-                    cmd.Dispose();
+                    using (MySqlCommand cmd = new MySqlCommand($"DELETE FROM workshifts WHERE userID=@userid AND date = @date AND workshift = @workshift", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@userid", employee.UserID);
+                        cmd.Parameters.AddWithValue("@date", date.Date);
+                        cmd.Parameters.AddWithValue("@workshift", workshiftIndex);
+                        cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+                    }
                 }
-                conn.Close();
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally { conn.Close(); }
             }
         }
     }
