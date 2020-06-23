29089458e026094051891fae3b2aba3859253521
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-06-23 04:57:31 +0200

    Changes: Applied issues #L001, #L002

--- MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/Views/LoginView.xaml.cs
+++ MediaBazaarApplicationWPF/MediaBazaarApplicationWPF/Views/LoginView.xaml.cs
@@ -30,7 +30,15 @@ namespace MediaBazaarApplicationWPF.Views
 
         }
 
-        
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            { 
+                ((dynamic)this.DataContext).Password = ((PasswordBox)sender).Password;
+            }
+        }
+
+
         //private void TestLogin_Click(object sender, RoutedEventArgs e)
         //{
         //    LoggedInUser.role = EmployeeRole.Owner;
